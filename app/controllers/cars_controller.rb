8f46ad57cff6269c6be4b46fca192907bb541e26
renanav <renan.avrahami@gmail.com>
2016-02-20 14:48:48 -0500

    Commiting

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -7,7 +7,7 @@ class CarsController < ApplicationController
   def category
     @info = params[:category]
     case params[:category]
-        when "sedan" then @cars = Car.where(category: "Sedan")
+        when "sedan" then @cars = Car.where(category: "sedan")
         when "suv" then @cars = Car.where(category: "suv")
         when "super_sport" then @cars = Car.where(category: "super_sport")
         when "super_saloon" then @cars = Car.where(category: "super_saloon")
