da3ce7ce61b8de92abad577aec423c7a44089b23
renanav <renan.avrahami@gmail.com>
2016-03-01 19:54:02 -0500

    Before first seeding

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-
+gem 'seed_dump'
 # gem 'therubyracer'
 # gem 'less-rails'
 # gem 'twitter-bootstrap-rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,85 +1,107 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-dealership1 = Dealership.create(name: "Renani's", address: "aventura, fl", email: "renan.avrahami@gmail.com", phone_number: "3058348384")
-
-
-s8_plus.image= "audi_s8_plus.jpeg"
-# category_id:1 is sedan
-# category_id:2 is suv
-# category_id:3 is super_sport
-# category_id:4 is super_saloon
-
-# make_id:1 is audi
-# make_id:2 is mercedes
-# make_id:3 is lexus
-# make_id:4 is land_rover
-# make_id:5 is bmw
-# ****** porsche is next but its id is 13
-# make_id:6 is volvo DELETE
-# make_id:7 is ferrari
-# make_id:8 is lamborghini
-# make_id:9 is bugatti
-# make_id:10 is rolls_roys
-# make_id:11 is bentley
-# make_id:12 is empty empty empty empty empty empty empty empty empty empty empty
-# make_id:13 is porsche
-# make_id:14 is aston_martin
-
-
-# make1 = Make.create(make: "audi")
-# make2 = Make.create(make: "mercedes")
-# make3 = Make.create(make: "lexus")
-# make4 = Make.create(make: "land_rover")
-# make5 = Make.create(make: "bmw")
-# make6 = Make.create(make: "volvo")
-# make7 = Make.create(make: "ferrari")
-# make8 = Make.create(make: "lamborghini")
-# make9 = Make.create(make: "bugatti")
-# make10 = Make.create(make: "rolls_roys")
-# make11 = Make.create(make: "bentley")
-# make12 = Make.create(make: "aston_martin")
-
-# car1 = Car.create(category: "sedan", make: "audi", model: "s8 plus", dealership_id:1)
-# car2 = Car.create(category: "sedan", make: "audi", model: "rs 7", dealership_id:1)
-# car3 = Car.create(category: "sedan", make: "audi", model: "s7", dealership_id:1)
-# car4 = Car.create(category: "sedan", make: "mercedes", model: "amg s65", dealership_id:1)
-# car5 = Car.create(category: "sedan", make: "mercedes", model: "amg s63", dealership_id:1)
-# car6 = Car.create(category: "sedan", make: "mercedes", model: "smercedes", dealership_id:1)
-# car7 = Car.create(category: "sedan", make: "lexus", model: "ls hybrid", dealership_id:1)
-# car8 = Car.create(category: "sedan", make: "lexus", model: "ls", dealership_id:1)
-# car9 = Car.create(category: "sedan", make: "lexus", model: "gsf", dealership_id:1)
-#
-# car10 = Car.create(category: "suv", make: "land_rover", model: "autobiography", dealership_id:1)
-# car11 = Car.create(category: "suv", make: "land_rover", model: "super_charged", dealership_id:1)
-# car12 = Car.create(category: "suv", make: "land_rover", model: "hse", dealership_id:1)
-# car13 = Car.create(category: "suv", make: "bmw", model: "ls", dealership_id:1)
-# car14 = Car.create(category: "suv", make: "bmw", model: "ls", dealership_id:1)
-# car15 = Car.create(category: "suv", make: "bmw", model: "ls", dealership_id:1)
-# car16 = Car.create(category: "suv", make: "volvo", model: "ls", dealership_id:1)
-# car17 = Car.create(category: "suv", make: "volvo", model: "ls", dealership_id:1)
-# car18 = Car.create(category: "suv", make: "volvo", model: "ls", dealership_id:1)
-#
-# car19 = Car.create(category: "super_sport", make: "volvo", model: "ls", dealership_id:1)
-# car20 = Car.create(category: "super_sport", make: "volvo", model: "ls", dealership_id:1)
-# car21 = Car.create(category: "super_sport", make: "volvo", model: "ls", dealership_id:1)
-# car22 = Car.create(category: "super_sport", make: "volvo", model: "ls", dealership_id:1)
-# car23 = Car.create(category: "super_sport", make: "volvo", model: "ls", dealership_id:1)
-# car24 = Car.create(category: "super_sport", make: "volvo", model: "ls", dealership_id:1)
-# car25 = Car.create(category: "super_sport", make: "volvo", model: "ls", dealership_id:1)
-# car26 = Car.create(category: "super_sport", make: "volvo", model: "ls", dealership_id:1)
-# car27 = Car.create(category: "super_sport", make: "volvo", model: "ls", dealership_id:1)
-#
-# car28 = Car.create(category: "super_saloon", make: "volvo", model: "ls", dealership_id:1)
-# car29 = Car.create(category: "super_saloon", make: "volvo", model: "ls", dealership_id:1)
-# car30 = Car.create(category: "super_saloon", make: "volvo", model: "ls", dealership_id:1)
-# car31 = Car.create(category: "super_saloon", make: "volvo", model: "ls", dealership_id:1)
-# car32 = Car.create(category: "super_saloon", make: "volvo", model: "ls", dealership_id:1)
-# car33 = Car.create(category: "super_saloon", make: "volvo", model: "ls", dealership_id:1)
-# car34 = Car.create(category: "super_saloon", make: "volvo", model: "ls", dealership_id:1)
-# car35 = Car.create(category: "super_saloon", make: "volvo", model: "ls", dealership_id:1)
-# car36 = Car.create(category: "super_saloon", make: "volvo", model: "ls", dealership_id:1)
+# Car.create!([
+#   {dealership_id: nil, model_id: nil},
+#   {dealership_id: nil, model_id: nil},
+#   {dealership_id: nil, model_id: nil},
+#   {dealership_id: nil, model_id: nil},
+#   {dealership_id: nil, model_id: nil},
+#   {dealership_id: nil, model_id: nil},
+#   {dealership_id: nil, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil},
+#   {dealership_id: 1, model_id: nil}
+# ])
+Category.create!([
+  {name: "sedan"},
+  {name: "suv"},
+  {name: "super_sport"},
+  {name: "super_saloon"}
+])
+Dealership.create!([
+  {name: "Renani's", address: "aventura, fl", email: "renan.avrahami@gmail.com", phone_number: "3058348384"},
+  # {name: "Renani's", address: "aventura, fl", email: "renan.avrahami@gmail.com", phone_number: "3058348384"}
+])
+Make.create!([
+  {name: "Audi"},
+  {name: "Mercedes"},
+  {name: "Lexus"},
+  {name: "Land Rover"},
+  {name: "Bmw"},
+  {name: "Ferrari"},
+  {name: "Lamborghini"},
+  {name: "Bugatti"},
+  {name: "Rolls Roys"},
+  {name: "Bentley"},
+  {name: "Aston Martin"},
+  {name: "Porsche"}
+])
+Model.create!([
+  {name: "s8_plus", make_id: 1, category_id: 1, image: "audi_s8_plus"}
+  {name: "rs_7_2016", make_id: 1, category_id: 1, image: "audi_rs7"},
+  {name: "s7_2016", make_id: 1, category_id: 1, image: "audi_s7"},
+  {name: "amg_s65_2016", make_id: 2, category_id: 1, image: "mercedes_amg_s65"},
+  {name: "amg_s63_2016", make_id: 2, category_id: 1, image: "mercedes_amg_s63"},
+  {name: "s600_2016", make_id: 2, category_id: 1, image: "mercedes_s600"},
+  {name: "ls_hybrid_2016", make_id: 3, category_id: 1, image: "lexus_ls_hybrid"},
+  {name: "ls_2016", make_id: 3, category_id: 1, image: "lexus_ls"},
+  {name: "gsf_2016", make_id: 3, category_id: 1, image: "lexus_gsf"},
+  {name: "sv_autobiography_2016", make_id: 4, category_id: 2, image: "land_rover_sv_autobiography"},
+  {name: "supercharged_2016", make_id: 4, category_id: 2, image: "land_rover_supercharged"},
+  {name: "hse_2016", make_id: 4, category_id: 2, image: "land_rover_hse"},
+  {name: "x6_m_2016", make_id: 5, category_id: 2, image: nil},
+  {name: "x5_m_2016", make_id: 5, category_id: 2, image: nil},
+  {name: "x4_m40i_2016", make_id: 5, category_id: 2, image: nil},
+  {name: "cayenne_turbo_s_2016", make_id: 13, category_id: 2, image: nil},
+  {name: "cayenne_turbo_2016", make_id: 13, category_id: 2, image: nil},
+  {name: "macan_2016", make_id: 13, category_id: 2, image: nil},
+  {name: "california_2016", make_id: 7, category_id: 3, image: nil},
+  {name: "488_gtb_2016", make_id: 7, category_id: 3, image: nil},
+  {name: "la_ferrari_2016", make_id: 7, category_id: 3, image: nil},
+  {name: "venono_roadster", make_id: 8, category_id: 3, image: nil},
+  {name: "aventador_superveloce_roadster_2016", make_id: 8, category_id: 3, image: nil},
+  {name: "huracan_2016", make_id: 8, category_id: 3, image: nil},
+  {name: "veyron_16.4_2016", make_id: 9, category_id: 3, image: nil},
+  {name: "veyron_16.4_grand_sport_2016", make_id: 9, category_id: 3, image: nil},
+  {name: "veyron_16.4_grand_sport_vitesse_2016", make_id: 9, category_id: 3, image: nil},
+  {name: "ghost_2016", make_id: 10, category_id: 4, image: nil},
+  {name: "phantom_2016", make_id: 10, category_id: 4, image: nil},
+  {name: "wraith_2016", make_id: 10, category_id: 4, image: nil},
+  {name: "mulsanne_2016", make_id: 11, category_id: 4, image: nil},
+  {name: "continental_gt3r_2016", make_id: 11, category_id: 4, image: nil},
+  {name: "flying_spur_2016", make_id: 11, category_id: 4, image: nil},
+  {name: "db9_volante_2016", make_id: 12, category_id: 4, image: nil},
+  {name: "vanquish_carbon_edition_2016", make_id: 12, category_id: 4, image: nil},
+  {name: "rapide_s_2016", make_id: 12, category_id: 4, image: nil},
+])
