82b8acb68ade00f3bc02abcb40df72b5478f4be0
renanav <renan.avrahami@gmail.com>
2016-04-26 01:47:31 -0400

    rake assets:precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,6 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.serve_static_assets = true
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-  config.assets.compile = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -31,6 +30,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
+  # cHANGED TO TRUE TO ENABLE IMAGE_TAGS
   config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
