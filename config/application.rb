94d69d05e46971c8d1f85faee268b9e3ac27fa32
renanav <renan.avrahami@gmail.com>
2016-04-26 00:26:19 -0400

    Relocate that code in the file

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,15 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-# To show image_tags on Heroku
-config.serve_static_assets = true
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Cars
   class Application < Rails::Application
+    # To show image_tags on Heroku
+    config.serve_static_assets = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
